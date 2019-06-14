fc4135940355a686d171d34e66ff372ba5cec52d
Leonardo Molina <leomolina0501@hotmail.com>
2019-06-14 17:45:00 -0300

    Criação Transactions

---
+++ DDDImovel.Infra/Transactions/Uow.cs
@@ -0,0 +1,29 @@
+﻿using DDDImovel.Infra.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDImovel.Infra.Transactions
+{
+    public class Uow : IUow
+    {
+        private readonly DDDImovelStoreDataContext _context;
+
+        public Uow(DDDImovelStoreDataContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Rollback()
+        {
+
+        }
+    }
+}
